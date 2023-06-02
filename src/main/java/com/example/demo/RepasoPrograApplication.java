70bcbe7aeca86ec530e13d96b5cd4635c64c1dbd
John <johnfe@outlook.es>
2023-06-02 10:02:02 -0500

    Primer upload al repo

---
+++ src/main/java/com/example/demo/RepasoPrograApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepasoPrograApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepasoPrograApplication.class, args);
+		System.out.println("Hola, primer commit");
+	}
+
+}
