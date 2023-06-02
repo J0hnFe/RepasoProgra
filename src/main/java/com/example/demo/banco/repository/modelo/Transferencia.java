2ceff3b61c285d80fdcf366342f8be5a564b9736
John <johnfe@outlook.es>
2023-06-02 10:14:07 -0500

    Añadiendo diferentes paquetes y clases

---
+++ src/main/java/com/example/demo/banco/repository/modelo/Transferencia.java
@@ -0,0 +1,5 @@
+package com.example.demo.banco.repository.modelo;
+
+public class Transferencia {
+
+}
---
+++ src/main/java/com/example/demo/banco/service/TransferenciaService.java
@@ -0,0 +1,5 @@
+package com.example.demo.banco.service;
+
+public interface TransferenciaService {
+
+}
